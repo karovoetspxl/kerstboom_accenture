035848bcec93978a16519fe56791ffbda300ef8c
KaroVoetsPXL <Karo.Voets@student.pxl.be>
2023-12-14 15:55:42 +0100

    javascript en positionering

---
+++ script.js
@@ -0,0 +1,44 @@
+//BALLS
+function colorBall1(z) {
+    let a = getComputedStyle(z);
+    let b = a.backgroundColor;
+    document.getElementsByClassName("ball1")[0].style.backgroundColor = b;
+    document.getElementsByClassName("ball1")[1].style.backgroundColor = b;
+    document.getElementsByClassName("ball1")[2].style.backgroundColor = b;
+}
+
+function colorBall2(z) {
+    let a = getComputedStyle(z);
+    let b = a.backgroundColor;
+    document.getElementsByClassName("ball2")[0].style.backgroundColor = b;
+    document.getElementsByClassName("ball2")[1].style.backgroundColor = b;
+    document.getElementsByClassName("ball2")[2].style.backgroundColor = b;
+}
+
+//LIGHTS
+function colorLight1(z) {
+    let a = getComputedStyle(z);
+    let b = a.backgroundColor;
+    document.getElementsByClassName("light1")[0].style.backgroundColor = b;
+    document.getElementsByClassName("light1")[1].style.backgroundColor = b;
+    document.getElementsByClassName("light1")[2].style.backgroundColor = b;
+    document.getElementsByClassName("light1")[3].style.backgroundColor = b;
+}
+
+function colorLight2(z) {
+    let a = getComputedStyle(z);
+    let b = a.backgroundColor;
+    document.getElementsByClassName("light2")[0].style.backgroundColor = b;
+    document.getElementsByClassName("light2")[1].style.backgroundColor = b;
+    document.getElementsByClassName("light2")[2].style.backgroundColor = b;
+    document.getElementsByClassName("light2")[3].style.backgroundColor = b;
+}
+
+function colorLight3(z) {
+    let a = getComputedStyle(z);
+    let b = a.backgroundColor;
+    document.getElementsByClassName("light3")[0].style.backgroundColor = b;
+    document.getElementsByClassName("light3")[1].style.backgroundColor = b;
+    document.getElementsByClassName("light3")[2].style.backgroundColor = b;
+    document.getElementsByClassName("light3")[3].style.backgroundColor = b;
+}
